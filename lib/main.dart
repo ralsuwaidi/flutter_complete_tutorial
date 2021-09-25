f5d9120e1af92d259412d2bc9a980b2124c079f4
rashed <x.suwaidi@gmail.com>
2021-09-25 08:27:30 +0400

    add question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print('Answer chosen');
+  }
+
   Widget build(BuildContext context) {
+    var _questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
+
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(_questions[_questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+          ],
+        ),
+      ),
     );
   }
 }
